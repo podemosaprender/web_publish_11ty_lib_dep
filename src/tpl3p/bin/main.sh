c33d0b4cf864ad58ccfb0c566bc1f3689fd93872
devel@mauriciocap.com <devel@mauriciocap.com>
2024-06-15 23:53:52 +0000

    OK: .sh para ejemplo -> plantilla

---
+++ src/tpl3p/bin/main.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#INFO: bajar html y assets, convertir a template
+
+P_URL=${1:-https://themesbrand.com/lezir/layout/layout-one-6.html}
+DLD="${P_URL##*//}"
+HTML="${DLD##*/}"
+if [ -f "$DLD" ]; then echo "no download needed" ;
+else 
+	wget -pk "$P_URL" #A baja en dir con assets ; 
+	find themesbrand.com/ -iname "*\?*" -exec bash -c 'i="{}"; mv "$i" "${i%%\?*}"' \;
+fi
+
+rm -Rf xo/ ; node bin/8to_template.js "$DLD"  2>&1 #A: separa en templates
+cp -r "${DLD%/*}"/* xo/page/ ; rm xo/page/$HTML
