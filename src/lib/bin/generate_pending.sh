64fa206850322d02277b795d8ee4410e6e17616a
h@podemosaprender.org <h@podemosaprender.org>
2024-07-18 10:24:11 +0000

    FIX: generate pushed

--- src/lib/bin/generate_pending.sh
+++ src/lib/bin/generate_pending.sh
@@ -10,7 +10,7 @@ echo "P_VAR_DIR=$P_VAR_DIR P_REPOS_DIR=$P_REPOS_DIR BASE_DIR=$BASE_DIR"
 echo "PENDING at $P_PENDING_DIR `find $P_PENDING_DIR -type f`"
 
 export P_VAR_DIR P_SITE_JSON_DIR P_REPOS_DIR
-for site_id_path in $P_PENDING_DIR/in/* ; do site_id=`basename $site_id_path`
+for site_id_path in $P_PENDING_DIR/in/${1:-*} ; do site_id=`basename $site_id_path`
 	echo "GENERATE $site_id"
 	#XXX:lock
 	$BASE_DIR/src/lib/bin/generate1.sh $site_id UPDATED
