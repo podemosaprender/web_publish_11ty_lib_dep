6dd317b62cfc8f0a0de967c115b773a0c9e752c4
devel@mauriciocap.com <devel@mauriciocap.com>
2024-07-23 23:00:18 +0000

    blogspot

--- src/lib/bin/import_blogspot.js
+++ src/lib/bin/import_blogspot.js
@@ -1,16 +1,26 @@
 //INFO: import from a blogspot url
 
+const yaml= require('js-yaml');
+
 async function urlToX(baseUrl) {
 	let url= baseUrl+'/feeds/posts/default?alt=json';
 	let json= await fetch(url).then(r => r.json())
-	let r= json.feed.entry.map(e => {
+	let r= {};
+	json.feed.entry.map(e => {
 		let title= e.title['$t']; //txt
+		let dt_pub= e.published['$t'];
 		let txt= e.content['$t']
 			.split(/<p[^>]*>/)
-			.map(inner => ('<p>'+inner.replace(/<[^>]*>/gsi,' ')+'</p>\n'))
+			.map(inner => (
+				inner
+					.replace(/<[^>]*>/gsi,' ')
+					.replace(/&nbsp;/gs,' ').replace(/&gt;/gs,'>').replace(/&lt;/gs,'<')
+					.replace(/\s+/gsi,' ')
+					+'\n\n'))
 			.join('');
+		r[title]= {title, dt_pub, txt};
 	});
-	console.log(JSON.stringify(r,0,2))
+	console.log(yaml.dump({items: r}))
 	return r;
 }
 
